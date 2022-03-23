d596055015f65bec83335b39fe822a2c9511d916
Paulo Pires <ppiresdev@gmail.com>
2022-03-23 16:57:50 -0300

    Feito CSS do carrinho de compras

--- js/sript.js
+++ js/sript.js
@@ -1,5 +1,14 @@
+/*Para exibir o barra de buscas */
 const formBusca = document.querySelector(".form-busca");
 
 document.querySelector("#busca-btn").onclick = () => {
   formBusca.classList.toggle("form-busca-ativo");
+};
+
+/*Para exibir o carrinho de compras */
+
+const carrinhoCompras = document.querySelector(".carrinho-compras");
+
+document.querySelector("#carrinho-compras-btn").onclick = () => {
+  carrinhoCompras.classList.toggle("ativo");
 };
